7eb39fbfbd1a61e8952090096ce014b62681fefc
TommyKriz <lizardman420davidicke.com@gmail.com>
2018-10-19 17:37:14 +0200

    ue08 fast fertig

--- src/main/java/assignment08/Solution.java
+++ src/main/java/assignment08/Solution.java
@@ -39,6 +39,8 @@ public class Solution implements PlugInFilter {
 
 		new ImagePlus("Color Distribution",
 				ColorDistribution.distribution(allColors)).show();
+		new ImagePlus("Color Distribution",
+				ColorDistribution.distribution(referenceColors)).show();
 
 	}
 
